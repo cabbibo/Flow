c7e4c8d46fbb74c2fbab72466a8387a4c1ee67d6
Isaac Cohen <icohen@leapmotion.com>
2014-04-15 00:32:50 -0700

    commiting new final version

--- shaders/fs-model.js
+++ shaders/fs-model.js
@@ -5,14 +5,23 @@ uniform vec3 color;
 
 varying vec4 vAudio;
 varying vec3 vColor;
+varying vec3 vNormal;
 
 const float pi = 3.14159;
 void main() {
 
-  vec4 c = texture2D( t_audio , vec2( abs(vColor.z) , 0.0 ) ); 
+  float cutoff = max( 0. ,  (vColor.y-.6) );
+  float c;
+  if( cutoff != 0. ){
+    c = texture2D( t_audio , vec2( cutoff , 0.0 ) ).x; 
 
-  float p = (power -1. )*(power-1.) / 4.;
-  gl_FragColor = vec4( p * ((c.xyz* .2)+color.xyz*.05), vAudio.w );
+  }else{
+    c = .6;
+  }
+
+  c = texture2D( t_audio , vec2( abs(vNormal.x) , 0.0 )).x;
+  float p = (power -1.5 )*(power-1.5) / 4.;
+  gl_FragColor = vec4(color*.1*p + (vec3(max(0.1 ,vNormal.x+.1))*p * c*c*c ), vAudio.w );
 
 }
 
